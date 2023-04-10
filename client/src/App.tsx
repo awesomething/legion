690caabbf2e6e51501912ecbca6bd072ed3e3033
awesomething <79168736+PairApp@users.noreply.github.com>
2023-04-10 14:15:54 -0400

    refine added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -60,7 +60,7 @@ function App() {
 
       //save users to MDB
       if(profileObj){
-        const response = await fetch('http://localhost:8080/api/v1/users', {
+        const response = await fetch('https://legion-a7eu.onrender.com/api/v1/users', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({
@@ -128,7 +128,7 @@ function App() {
       <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
       <RefineSnackbarProvider>
         <Refine
-          dataProvider={dataProvider("http://localhost:8080/api/v1")}//https://api.fake-rest.refine.dev
+          dataProvider={dataProvider("https://legion-a7eu.onrender.com")}//https://api.fake-rest.refine.dev
           notificationProvider={notificationProvider}
           ReadyPage={ReadyPage}
           catchAll={<ErrorComponent />}
